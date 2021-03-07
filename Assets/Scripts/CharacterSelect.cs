a0aca35c75c14dbd70b2fbc7cb3ad02163ec0302
Jay Helm <45608814+SgtBacon@users.noreply.github.com>
2021-03-07 14:16:21 -0800

    Initial commit

---
+++ Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class CharacterSelect : MonoBehaviour
+{
+    public GameObject LevelSelectPanel;   //Open new panel for level select to lessen number of times objects are saved between scenes
+    public GameObject CharacterSelectPanel;
+    public GameObject SelectedChar;
+
+    //Player picks character
+    //Call Auto_Selection and Pass that character into Auto_Selection
+    //Therefore cause game to pick random character, excluding random character
+    //Load an overlay for picking level, send both characters to chosen level
+    public void PlayerSelect(Button b)
+    {
+        Auto_Select(b.name);
+    }
+    public void Auto_Select(string SelectedChar)
+    {
+        //Create an array of all characters
+        //Remove character selected by Player/Lessen chance of them being selected by computer
+        OpenLevelSelect();
+    }
+    public void OpenLevelSelect()
+    {
+        if (CharacterSelectPanel.activeInHierarchy)
+        {
+            CharacterSelectPanel.SetActive(false);
+            LevelSelectPanel.SetActive(true);
+        }
+        else
+        {
+            LevelSelectPanel.SetActive(false);
+            CharacterSelectPanel.SetActive(true);
+        }
+    }
+
+    public void SelectLevel(Button b)
+    {
+        SceneManager.LoadScene(b.name);
+    }
+}
---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    //On press, start game
+    public void StartGame()
+    {
+        SceneManager.LoadScene("CharacterSelect");
+    }
+
+    //On press, Quit Game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    //On press, open Options
+    public void Options()
+    {
+        //SceneManager.LoadScene("Options");
+        Debug.Log("Options");
+    }
+}
