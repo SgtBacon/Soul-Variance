4c488ef485fb6dc16ba357e8a6aea24b2cbd7ebc
Jay Helm <45608814+SgtBacon@users.noreply.github.com>
2021-03-10 11:16:29 -0800

    Updated basic character movement

--- Assets/Chars/Char1.cs
+++ Assets/Chars/Char1.cs
@@ -8,10 +8,12 @@ public class Char1 : MonoBehaviour
     public string Name;
     Rigidbody2D rb;
     public float speed;
+    public Vector2 JumpHeight;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
@@ -20,13 +22,19 @@ public class Char1 : MonoBehaviour
         Move();
     }
 
+
+    //Move Function called by Update(), moved for readability
+    //Update function when adding Options menu so that User can change their button mapping
     public void Move()
     {
         float x = Input.GetAxis("Left_Right");
         float MoveBy = x * speed;
         rb.velocity = new Vector2(MoveBy, rb.velocity.y);
 
-        float jump = Input.GetAxis("Jump");
         
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(JumpHeight, ForceMode2D.Impulse);
+        }
     }
 }
